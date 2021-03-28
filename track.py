79ad7fdd0c59d798b95c44f653c6004f12ba8535
zhiyuan454 <793538304@qq.com>
2021-03-28 20:16:18 +0800

    line

---
+++ track.py
@@ -0,0 +1,345 @@
+import sys
+sys.path.insert(0, './yolov5')
+
+from yolov5.utils.datasets import LoadImages, LoadStreams
+from yolov5.utils.general import check_img_size, non_max_suppression, scale_coords
+from yolov5.utils.torch_utils import select_device, time_synchronized
+from deep_sort_pytorch.utils.parser import get_config
+from deep_sort_pytorch.deep_sort import DeepSort
+import argparse
+import os
+import platform
+import shutil
+import time
+from pathlib import Path
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+
+
+#############################大门的俩点
+line_x1=420
+line_y1=490
+line_x2=780
+line_y2=390
+#####################################
+# line_x1=330
+# line_y1=270
+# line_x2=680
+# line_y2=490
+################################胖姐烧烤
+# line_x1=380
+# line_y1=390
+# line_x2=680
+# line_y2=420
+
+person_dict={}# id:
+customer_number = 0
+def check_person(x,y):#判断人是否越线
+    x = float(x)
+    y = float(y)
+    if(x<min(line_x1,line_x2) or x>max(line_x1,line_x2)):
+        return -1
+    if line_x2 - line_x1 == 0:
+        bo = line_y1 - y
+    else:
+        bo = line_y1 + (line_y2 - line_y1) / (line_x2 - line_x1) * (x - line_x1) - y  # 两点式
+    if (bo >= 0):
+        return 1
+    else:
+        return 0
+######划线
+def draw_line(img,x1,y1,x2,y2):
+    cv2.line(img,(x1,y1),(x2,y2),(255,0,0),5)
+########################################################
+palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
+def bbox_rel(*xyxy):
+    """" Calculates the relative bounding box from absolute pixel values. """
+    bbox_left = min([xyxy[0].item(), xyxy[2].item()])
+    bbox_top = min([xyxy[1].item(), xyxy[3].item()])
+    bbox_w = abs(xyxy[0].item() - xyxy[2].item())
+    bbox_h = abs(xyxy[1].item() - xyxy[3].item())
+    x_c = (bbox_left + bbox_w / 2)
+    y_c = (bbox_top + bbox_h / 2)
+    w = bbox_w
+    h = bbox_h
+    return x_c, y_c, w, h
+
+
+def compute_color_for_labels(label):
+    """
+    Simple function that adds fixed color depending on the class
+    """
+    color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
+    return tuple(color)
+
+
+def draw_boxes(img, bbox, identities=None, offset=(0, 0)):
+    for i, box in enumerate(bbox):
+        x1, y1, x2, y2 = [int(i) for i in box]
+        x1 += offset[0]
+        x2 += offset[0]
+        y1 += offset[1]
+        y2 += offset[1]
+        # box text and bar
+        id = int(identities[i]) if identities is not None else 0
+        color = compute_color_for_labels(id)
+        label = '{}{:d}'.format("", id)
+        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
+        cv2.rectangle(
+            img, (x1, y1), (x1 + t_size[0] + 3, y1 + t_size[1] + 4), color, -1)
+        cv2.putText(img, label, (x1, y1 +
+                                 t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 2)
+        ###################################################################
+        # 画圆    所在图像    位置       半径     颜色          宽度       线型
+        cv2.circle(img,(int((x1+x2)/2),y2), 3 , (0, 0, 255), 3, cv2.LINE_8)#脚下画点
+        ch1=check_person(int((x1+x2)/2),y2)
+        if(id in person_dict):
+            if(person_dict[id][0]==0 and ch1 ==1):
+                global customer_number
+                customer_number+=1
+                person_dict[id][1] += 1
+            elif(person_dict[id][0] == 1 and ch1 == 0):
+                if(person_dict[id][1]!=0):
+                    customer_number -= 1
+                person_dict[id][2] += 1
+            person_dict[id][0] = ch1
+        else:
+            person_dict[id]=[ch1,0,0]
+        ###################################################################
+    return img
+
+
+def detect(opt, save_img=False):
+    # 获取输出文件夹，输入源，权重，参数等参数
+    out, source, weights, view_img, save_txt, imgsz = \
+        opt.output, opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
+    webcam = source == '0' or source.startswith(
+        'rtsp') or source.startswith('http') or source.endswith('.txt')
+
+    # initialize deepsort
+    cfg = get_config()
+    cfg.merge_from_file(opt.config_deepsort)
+    deepsort = DeepSort(cfg.DEEPSORT.REID_CKPT,
+                        max_dist=cfg.DEEPSORT.MAX_DIST, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
+                        nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
+                        max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
+                        use_cuda=True)
+
+    # Initialize
+    device = select_device(opt.device)
+    if os.path.exists(out):
+        shutil.rmtree(out)  # delete output folder
+    os.makedirs(out)  # make new output folder
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = torch.load(weights, map_location=device)[
+        'model'].float()  # load to FP32
+    model.to(device).eval()
+    if half:
+        model.half()  # to FP16
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    if webcam:
+        view_img = True
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadStreams(source, img_size=imgsz)
+    else:
+        view_img = True
+        save_img = True
+        dataset = LoadImages(source, img_size=imgsz)
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+
+    # Run inference
+    t0 = time.time()
+    img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
+    # run once
+    _ = model(img.half() if half else img) if device.type != 'cpu' else None
+
+    save_path = str(Path(out))
+    txt_path = str(Path(out)) + '/results.txt'
+
+    for frame_idx, (path, img, im0s, vid_cap) in enumerate(dataset):
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        # 没有batch_size的话则在最前面添加一个轴
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        """
+        前向传播 返回pred的shape是(1, num_boxes, 5+num_class)
+        h,w为传入网络图片的长和宽，注意dataset在检测时使用了矩形推理，所以这里h不一定等于w
+        num_boxes = h/32 * w/32 + h/16 * w/16 + h/8 * w/8
+        pred[..., 0:4]为预测框坐标
+        预测框坐标为xywh(中心点+宽长)格式
+        pred[..., 4]为objectness置信度
+        pred[..., 5:-1]为分类结果
+        """
+        t1 = time_synchronized()
+        pred = model(img, augment=opt.augment)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(
+            pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        t2 = time_synchronized()
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            # 如果输入源是webcam，则batch_size不为1，取出dataset中的一张图片
+            if webcam:  # batch_size >= 1
+                p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
+            else:
+                p, s, im0 = path, '', im0s
+
+            s += '%gx%g ' % img.shape[2:]  # print string
+            # 设置保存图片/视频的路径
+            save_path = str(Path(out) / Path(p).name)
+            #############################################
+            # global frame_number
+            # frame_number+=1
+            #
+            #############################################
+            if det is not None and len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(
+                    img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += '%g %ss, ' % (n, names[int(c)])  # add to string
+
+                bbox_xywh = []
+                confs = []
+
+                # Adapt detections to deep sort input format
+                for *xyxy, conf, cls in det:
+                    x_c, y_c, bbox_w, bbox_h = bbox_rel(*xyxy)
+                    obj = [x_c, y_c, bbox_w, bbox_h]
+                    bbox_xywh.append(obj)
+                    confs.append([conf.item()])
+
+                xywhs = torch.Tensor(bbox_xywh)
+                confss = torch.Tensor(confs)
+
+                # Pass detections to deepsort
+                outputs = deepsort.update(xywhs, confss, im0)
+
+                # draw boxes for visualization
+                if len(outputs) > 0:
+                    bbox_xyxy = outputs[:, :4]
+                    identities = outputs[:, -1]
+                    draw_boxes(im0, bbox_xyxy, identities)
+                # Write MOT compliant results to file
+                if save_txt and len(outputs) != 0:
+                    for j, output in enumerate(outputs):
+                        bbox_left = output[0]
+                        bbox_top = output[1]
+                        bbox_w = output[2]
+                        bbox_h = output[3]
+                        identity = output[-1]
+                        with open(txt_path, 'a') as f:
+                            f.write(('%g ' * 10 + '\n') % (frame_idx, identity, bbox_left,
+                                                           bbox_top, bbox_w, bbox_h, -1, -1, -1, -1))  # label format
+
+            else:
+                deepsort.increment_ages()
+            ######################################划线  统计
+            draw_line(im0,line_x1,line_y1,line_x2,line_y2)
+            cv2.putText(im0, r"customer:"+str(customer_number), (10, 50), cv2.FONT_HERSHEY_PLAIN, 4, (204, 255, 204), 6)
+            ######################################
+            # Print time (inference + NMS)
+            print('%sDone. (%.3fs)' % (s, t2 - t1))
+
+            # Stream results
+            if view_img:
+                cv2.imshow(p, im0)
+                if cv2.waitKey(1) == ord('q'):  # q to quit
+                    raise StopIteration
+
+            # Save results (image with detections)
+            if save_img:
+                print('saving img!')
+                if dataset.mode == 'images':
+                    cv2.imwrite(save_path, im0)
+                else:
+                    print('saving video!')
+                    if vid_path != save_path:  # new video
+                        vid_path = save_path
+                        if isinstance(vid_writer, cv2.VideoWriter):
+                            vid_writer.release()  # release previous video writer
+
+                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        vid_writer = cv2.VideoWriter(
+                            save_path, cv2.VideoWriter_fourcc(*opt.fourcc), fps, (w, h))
+                    vid_writer.write(im0)
+
+    if save_txt or save_img:
+        print('Results saved to %s' % os.getcwd() + os.sep + out)
+        if platform == 'darwin':  # MacOS
+            os.system('open ' + save_path)
+
+    print('Done. (%.3fs)' % (time.time() - t0))
+
+    """
+    weights:训练的权重
+    source:测试数据，可以是图片/视频路径，也可以是'0'(电脑自带摄像头),也可以是rtsp等视频流
+    output:网络预测之后的图片/视频的保存路径
+    img-size:网络输入图片大小
+    conf-thres:置信度阈值
+    iou-thres:做nms的iou阈值
+    device:设置设备
+    view-img:是否展示预测之后的图片/视频，默认False
+    save-txt:是否将预测的框坐标以txt文件形式保存，默认False
+    classes:设置只保留某一部分类别，形如0或者0 2 3
+    agnostic-nms:进行nms是否也去除不同类别之间的框，默认False
+    augment:推理的时候进行多尺度，翻转等操作(TTA)推理
+    update:如果为True，则对所有模型进行strip_optimizer操作，去除pt文件中的优化器等信息，默认为False
+    """
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str,
+                        default='yolov5/weights/yolov5s.pt', help='model.pt path')
+    # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str,
+                        default='inference/images', help='source')
+    parser.add_argument('--output', type=str, default='inference/output',
+                        help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=640,
+                        help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float,
+                        default=0.15, help='object confidence threshold')#0.4
+    parser.add_argument('--iou-thres', type=float,
+                        default=1.0, help='IOU threshold for NMS')#0.5
+    parser.add_argument('--fourcc', type=str, default='mp4v',
+                        help='output video codec (verify ffmpeg support)')
+    parser.add_argument('--device', default='',
+                        help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true',
+                        help='display results')
+    parser.add_argument('--save-txt', action='store_true',
+                        help='save results to *.txt')
+    # class 0 is person
+    parser.add_argument('--classes', nargs='+', type=int,
+                        default=[0], help='filter by class')
+    parser.add_argument('--agnostic-nms', action='store_true',
+                        help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true',
+                        help='augmented inference')
+    parser.add_argument("--config_deepsort", type=str,
+                        default="deep_sort_pytorch/configs/deep_sort.yaml")
+    args = parser.parse_args()
+    args.img_size = check_img_size(args.img_size)
+    print(args)
+
+    with torch.no_grad():
+        detect(args)
